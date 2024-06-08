06cc50984ec8e94a447e72f079d20b05bdb951bd
JuanGuimaraes <juandasmerces@outlook.com>
2024-06-08 17:40:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive } from 'vue';
+
+const campos = reactive({
+  campoA: 0,
+  campoB: 0,
+  operador: '',
+  resultado: 0,
+})
+
+const somar = (campoA, campoB) => {
+  resultado = campoA + campoB;
+  return;
+  
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Calculadora</h1>
+    
+    <input @keyup="evento => campos.campoA = evento.target.value" type="number" placeholder="Digite um numero">
+    <input @keyup="evento => campos.campoB = evento.target.value" type="number" placeholder="Digite um numero">
+    <select @change="operador">
+    <option v-on:input={somar}>Somar</option>
+    <option v-on:click ={subtrair}>Subtrair</option>
+    <option v-on:click={multiplicar}>Multiplicar</option>
+    <option v-on:click={dividir}>Dividir</option>
+    </select>
+    
+  </div>
+  <div>
+    O resultado é {{ resultado }}
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
