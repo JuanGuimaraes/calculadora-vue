56785d706c1bdc69588bfcbb63f116dfa09e6afb
JuanGuimaraes <juandasmerces@outlook.com>
2024-06-10 22:48:46 -0300

    refazendo o codigo, com as correções

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,70 @@
-<script setup>
-import { reactive } from 'vue';
+<script>
 
-const campos = reactive({
-  campoA: 0,
-  campoB: 0,
-  operador: '',
-  resultado: 0,
-})
-
-const somar = (campoA, campoB) => {
-  resultado = campoA + campoB;
-  return;
-  
-}
-
+export default {
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: 'soma',
+      resultado: 0
+    };
+  },
+  methods: {
+    calcular() {
+      switch (this.operacao) {
+        case 'soma':
+          this.resultado = parseFloat(this.numero1) + parseFloat(this.numero2);
+          break;
+        case 'subtracao':
+          this.resultado = parseFloat(this.numero1) - parseFloat(this.numero2);
+          break;
+        case 'multiplicacao':
+          this.resultado = parseFloat(this.numero1) * parseFloat(this.numero2);
+          break;
+        case 'divisao':
+          this.resultado = parseFloat(this.numero1) / parseFloat(this.numero2);
+          break;
+      }
+    }
+  }
+};
 </script>
 
 <template>
   <div class="container">
-    <h1>Calculadora</h1>
-    
-    <input @keyup="evento => campos.campoA = evento.target.value" type="number" placeholder="Digite um numero">
-    <input @keyup="evento => campos.campoB = evento.target.value" type="number" placeholder="Digite um numero">
-    <select @change="operador">
-    <option v-on:input={somar}>Somar</option>
-    <option v-on:click ={subtrair}>Subtrair</option>
-    <option v-on:click={multiplicar}>Multiplicar</option>
-    <option v-on:click={dividir}>Dividir</option>
-    </select>
-    
+    <div class="campos">
+      <input class="inputs" type="number" v-model="numero1" @input="calcular">
+        <select class="seletor" v-model="operacao" @change="calcular">
+          <option value="soma">+</option>
+          <option value="subtracao">-</option>
+          <option value="multiplicacao">*</option>
+          <option value="divisao">/</option>
+        </select>
+      <input class="inputs" type="number" v-model="numero2" @input="calcular">
+    <div>Resultado: {{ resultado }}</div>
+    </div>
   </div>
-  <div>
-    O resultado é {{ resultado }}
-  </div>
-  
 </template>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+.container {
+  background-color:rgba(199, 236, 238,1.0) ;
+  width: 100%;
+  height: 100%;
+}
 
+.campos {
+  margin: 50px auto;
+  text-align: center;
+
+}
+.inputs, .seletor {
+  margin: 10px;
+  padding: 4px;
+}
 </style>
